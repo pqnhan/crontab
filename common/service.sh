d9afea991622937e4ad7e6f7b5a22959981c2e11
liangsai12 <liangsai12@sina.com>
2019-03-24 09:06:47 +0800

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -8,7 +8,7 @@ MODDIR=${0%/*}
 # TimeZoon 8,China. I do not known how to use tzselect
 export TZ=Asia/Shanghai
 #
-mkdir -p /data/crontabs 
+[ -d /data/crontabs ] && exit 0 || mkdir -p /data/crontabs 
 touch /data/crontabs/root
 
 #
