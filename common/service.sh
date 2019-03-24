16655ba8d10a9086fd5b201b71eff944052ac223
liangsai12 <liangsai12@sina.com>
2019-03-24 09:27:58 +0800

    Update service.sh
    /data/media/0/cron

--- common/service.sh
+++ common/service.sh
@@ -8,21 +8,21 @@ MODDIR=${0%/*}
 # TimeZoon 8,China. I do not known how to use tzselect
 export TZ=Asia/Shanghai
 #
-[ -d /data/crontabs ] && exit 0 || mkdir -p /data/crontabs 
-touch /data/crontabs/root
+[ -d /data/media/0/cron/crontabs ] && exit 0 || mkdir -p /data/media/0/cron/crontabs 
+touch /data/media/0/cron/crontabs/root
 
 #
 #a shell file 
-mkdir -p /data/cronsh 
-touch /data/cronsh/zd.sh
+mkdir -p /data/media/0/cron/cronsh 
+touch /data/media/0/cron/cronsh/zd.sh
 #mktouch /data/cronsh/zd.sh 2</dev/null
-chmod 766 /data/cronsh/zd.sh 2</dev/null
+chmod 766 /data/media/0/cron/cronsh/zd.sh 2</dev/null
 #cron start 
 #cron enable
 #date > /data/cron.log;
 
 #change the path
-crond -c /data/crontabs 
+crond -c /data/media/0/cron/crontabs 
 #crond -l 1-L /sdcard/cron_error.log
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
